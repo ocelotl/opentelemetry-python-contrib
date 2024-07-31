3434e6639ae2c84e8e3b3c1581a16d1d97cfa524
Diego Hurtado <ocelotl@users.noreply.github.com>
2024-07-31 16:16:45 -0600

    wip

--- fixer.py
+++ fixer.py
@@ -1,19 +1,30 @@
 from os import walk, getcwd
 from os.path import join
+from tomlkit import parse, dumps
 
 
-def find_pyproject_files(start_dir):
-    pyproject_files = []
-    for dirpath, dirnames, filenames in walk(start_dir):
-        for filename in filenames:
-            if filename == "pyproject.toml":
-                full_path = join(dirpath, filename)
-                pyproject_files.append(full_path)
-    return pyproject_files
+pyproject_file_paths = []
 
+for dirpath, dirnames, filenames in walk(getcwd()):
+    for filename in filenames:
+        if filename == "pyproject.toml":
+            pyproject_file_paths.append(join(dirpath, filename))
 
-if __name__ == "__main__":
-    current_dir = getcwd()  # Get the current directory
-    pyproject_files = find_pyproject_files(current_dir)
-    for file in pyproject_files:
-        print(file)
+for pyproject_file_path in pyproject_file_paths:
+    if "_template" in pyproject_file_path:
+        continue
+
+    with open(pyproject_file_path) as pyproject_file:
+
+        pyproject = parse(pyproject_file.read())
+
+        if "hatch" not in pyproject["tool"].keys():
+            continue
+
+    pyproject["tool"]["hatch"]["version"]["path"] = (
+        pyproject["tool"]["hatch"]["version"]["path"]
+        .replace(".py", "/__init__.py")
+    )
+
+    with open(pyproject_file_path, "w") as pyproject_file:
+        pyproject_file.write(dumps(pyproject))
